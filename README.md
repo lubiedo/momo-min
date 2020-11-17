36385339498955575fb5fc01dc84c0728fa0112f
lubiedo <lisandro.ubiedo@gmail.com>
2020-11-17 14:46:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# momo-min
+
+Grayscale all the web! ([Min](https://minbrowser.org/) version)
+
+More info: https://github.com/lubiedo/momo
+
+# Installation
+
+1. Create the `userscript` folder inside your Min browser data folder.
+2. Enable user scripts in Settings.
+3. Download `momo.js` and place it in your `userscript` folder.
+
---
+++ momo.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name momo
+// @match *
+// @run-at document-start
+// ==/UserScript==
+
+if (!window.momo) {
+  window.momo = {
+    enabled: false,
+     url_re: /^https*\:\/\//i,
+     toggle: function() {
+       var htmls   = document.getElementsByTagName('html')
+       var enabled = window.momo.enabled = !window.momo.enabled
+       for (let i = 0; i < htmls.length; i++) {
+         if (window.momo.url_re.test(document.location.href) === true) {
+           if (enabled)
+             htmls[i].style.setProperty("filter", "grayscale(100%)")
+           else
+             htmls[i].style.removeProperty("filter")
+         } else {
+           console.console.warn("momo: URL not http/https!")
+         }
+       }
+    }
+  }
+}
+
+// keyshortcut
+// tabs already open should be reloaded
+document.addEventListener('keyup', function (event) {
+    if (event.ctrlKey && event.shiftKey && event.key === 'G')
+        window.momo.toggle();
+});
